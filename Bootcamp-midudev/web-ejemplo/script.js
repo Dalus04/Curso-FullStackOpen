88bdf1e1fb9527c7b32eedbb89fe7d93eb2f62d5
Dalus04 <danielorlandosuarez14@gmail.com>
2024-01-27 20:29:33 -0500

    Parte 1(midudev) - JS

---
+++ Bootcamp-midudev/web-ejemplo/script.js
@@ -0,0 +1,22 @@
+console.log(restar(2,2))
+
+const list = []
+const anotherList = list.concat(100)
+
+const persona = {
+    name: 'Daniel',
+    username: '@Dalus',
+    age: 19,
+    isDev: true,
+    links: ['https://github.com/Dalus04/Curso-FullStackOpen']
+}
+
+const sumar = (a, b) => {
+    return a + b
+}
+
+function restar (a, b){
+    return a - b
+}
+
+console.log(sumar(2,2))
